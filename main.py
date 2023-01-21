b3b94dbe9d0cab005246be09cfce581248f80cbb
GizatPP <120188378+GizatPP@users.noreply.github.com>
2023-01-21 13:17:49 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,61 @@
+#if 26 > 25:
+# print("Yes, it's right")
+#It's comment and I can write here anything
+"""It's also comment"""
+
+#x = 5
+#kdsda = "It's text of type string"
+#print(x)
+#print(kdsda)
+
+#THISDD34saf_d = "This is weird variable"
+#print(THISDD34saf_d)
+#x, y, z = "They are equal", "Orange", "Apple"
+#print(x)
+#print(y)
+#print(z)
+#x = y = z = "They are also equal"
+#print(x)
+#print(y)
+#print(z)
+#print(x + y + z)
+
+#def myFunc():
+    #print("It's a function ")
+
+#myFunc()
+#x = "String"
+#print(type(x))
+#x = 45
+#print(type(x))
+#y = {"color" : "red", "number" : 489}
+#print(y)
+
+#a = 3.14564
+#print(type(a))
+#var = 45
+#print(var)
+#n = str(var)
+#print(type(n))
+#text = "Hello world!"
+#print(text[0])
+#for i in text:
+    #print(i)
+#print(len(text))
+#print("Hello" in text)
+#if "world!" in text:
+    #print("Yes")
+#print(text[2:6])
+#print(text[-1])
+#print(text[-4:-1])
+#print(text.upper())
+#print(text.lower())
+#print(text.split())
+#print(text.strip())
+#print(text.replace("o","LO"))
+#city = "Almaty"
+#new = "I live in {}"
+#print(new.format(city))
+
+#import math
+#print(math.sqrt(256) > 15)
